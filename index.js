8d0c36afef139f64f0feac414fd66187a4bc7c28
Tomas Hubelbauer <tomas@hubelbauer.net>
2020-03-04 20:37:17 +0100

    Implement converting files to a base 64 data URI

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ window.addEventListener('load', async () => {
   /** @type {HTMLTextAreaElement} */
   const sourceTextArea = document.getElementById('sourceTextArea');
 
+  /** @type {HTMLInputElement} */
+  const fileInput = document.getElementById('fileInput');
+
+  /** @type {HTMLButtonElement} */
+  const clearButton = document.getElementById('clearButton');
+
+  /** @type {HTMLSpanElement} */
+  const creditSpan = document.getElementById('creditSpan');
+
   /** @type {HTMLTextAreaElement} */
   const resultTextArea = document.getElementById('resultTextArea');
 
@@ -17,13 +26,51 @@ window.addEventListener('load', async () => {
   /** @type {HTMLInputElement} */
   const hugInput = document.getElementById('hugInput');
 
+  // Prevent the browser from recovering the last selected file
+  fileInput.value = '';
+
   sourceTextArea.addEventListener('input', go);
+  fileInput.addEventListener('change', go);
   typeInput.addEventListener('input', go);
   charsetSelect.addEventListener('change', go);
   strideInput.addEventListener('input', go);
   hugInput.addEventListener('input', go);
+  clearButton.addEventListener('click', () => {
+    fileInput.value = '';
+    go();
+  });
 
   function go() {
+    // Hide the credit the moment the user changes the demo source value
+    creditSpan.style.display = 'none';
+
+    if (fileInput.files.length > 0) {
+      if (fileInput.files.length > 1) {
+        alert('Multiple files are not supported, taking the first file.');
+      }
+
+      const file = fileInput.files[0];
+
+      clearButton.disabled = false;
+      sourceTextArea.value = '';
+      sourceTextArea.disabled = true;
+      typeInput.value = file.type;
+      typeInput.disabled = true;
+      charsetSelect.value = 'base64';
+      charsetSelect.disabled = true;
+
+      const fileReader = new FileReader();
+      fileReader.addEventListener('load', () => goResult(fileReader.result));
+      fileReader.addEventListener('error', () => alert('Failed to load the file.'));
+      fileReader.readAsDataURL(file);
+      return;
+    }
+
+    clearButton.disabled = true;
+    sourceTextArea.disabled = false;
+    typeInput.disabled = false;
+    charsetSelect.disabled = false;
+
     let result = 'data:';
     result += typeInput.value;
     if (charsetSelect.value) {
@@ -37,6 +84,10 @@ window.addEventListener('load', async () => {
       result += ',' + sourceTextArea.value;
     }
 
+    goResult(result);
+  }
+
+  function goResult(result) {
     let lines = [result];
     if (strideInput.value) {
       lines = result.match(new RegExp(`.{0,${strideInput.valueAsNumber}}`, 'g'));
--- readme.md
+++ readme.md
@@ -5,5 +5,3 @@ A tool for generating Base64 data URIs.
 Example SVG icon by [Freepik](https://www.freepik.com).
 
 ## To-Do
-
-### Add support for loading files to encode in data URLs
